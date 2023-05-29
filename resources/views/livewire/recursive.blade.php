ff8846f72903fd80e24fc98cd4f8856e1f4b57cf
Devinson Alvarez <dalvarez@sistemasmitdev.com>
2023-05-29 14:28:15 -0500

    Created Tree view full component

--- resources/views/livewire/recursive.blade.php
+++ resources/views/livewire/recursive.blade.php
@@ -1,17 +1,16 @@
 <div class="bg-slate-100 shadow-md p-3 m-3 ml-10 border border-indigo-500 shadow-indigo-500" x-data="{ open: false }">
     <x-button @click="open = ! open">
-        {{ $parent->name }}
-        {{ $parent->id }}
+        Expand
     </x-button>
 
     <x-button wire:click="getData({{ $parent->id }})">
-        data
+        {{ $parent->name }}
     </x-button>
 
     @if ($parent->children)
         <div x-show="open">
             @foreach ($parent->children as $parent)
-                @livewire('recursive', ['parent' => $parent])
+                @livewire('recursive', ['parent' => $parent, key($parent->id)]) {{-- ! The key must be passed --}}
             @endforeach
         </div>
     @endif
